ba9a2f94a367f1cbccd6ebbc5c6fd5e3b5c8187e
JHONY-X <ermiasdereje24@gmail.com>
2025-06-22 17:01:59 +0300

    Add files via upload

---
+++ minigit_fileutils.cpp
@@ -0,0 +1,14 @@
+// MiniGit: Enhanced Version Control System
+// Features: commit list, FileUtils integration
+// Language: C++17
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <ctime>
+#include <string>
+#include <algorithm>
+#include "FileUtils.h" 
